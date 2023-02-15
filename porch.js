d1f792a33c22566459540bfc47cea0318f1c2a01
PxperMoon <89617671+PxperMoon@users.noreply.github.com>
2023-02-14 22:51:46 -0800

    Add files via upload

---
+++ porch.js
@@ -0,0 +1,29 @@
+status = "";
+
+function preload() {
+    porch = loadImage("porch.jpeg");
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+}
+
+function modelLoaded() {
+console.log("CocoSSD model is loaded.");
+}
+
+function setup() {
+    canvas = createCanvas(480, 380);
+    canvas.center();
+    background("white");
+}
+
+function draw() {
+
+}
+
+
+function gotResult(error, results) {
+    if(error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
