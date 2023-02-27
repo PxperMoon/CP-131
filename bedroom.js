b30bd6c2d4cae2525b7942f86c684a1b68b28ad5
PxperMoon <89617671+PxperMoon@users.noreply.github.com>
2023-02-26 18:31:48 -0800

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,43 @@
+status = "";
+objects = [];
+
+function preload() {
+    bedroom = loadImage("bedroom.jpeg");
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+}
+
+function modelLoaded() {
+    console.log("CocoSSD model is loaded.");
+    status = true;
+    objectDetector.detect(bedroom, gotResult);
+}
+
+function setup() {
+    canvas = createCanvas(480, 380);
+    canvas.center();
+    background("white");
+}
+
+function draw() {
+    if(status != "") {
+        for(i = 0;i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+
+            fill("#C1666B");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#C1666B");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+
+function gotResult(error, results) {
+    if(error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
--- bookshelf.js
+++ bookshelf.js
@@ -7,16 +7,29 @@ function preload() {
 
 function modelLoaded() {
 console.log("CocoSSD model is loaded.");
+status = true;
+objectDetector.detect(bookshelf, gotResult);
 }
 
 function setup() {
-    canvas = createCanvas(480, 380);
-    canvas.center();
-    background("white");
+canvas = createCanvas(480, 380);
+canvas.center();
+background("white");
 }
 
 function draw() {
+if(status != "") {
+    for(i = 0;i < objects.length; i++) {
+        document.getElementById("status").innerHTML = "Status: Objects Detected";
 
+        fill("#C1666B");
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+        noFill();
+        stroke("#C1666B");
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+    }
+}
 }
 
 
